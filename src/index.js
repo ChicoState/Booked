2e74254edd20540f1dee79b6027da808fdcaba04
daniel <danielv1777@yahoo.com>
2020-10-09 18:56:49 -0700

    attached backend

--- src/index.js
+++ src/index.js
@@ -2,12 +2,50 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+// import firebase from 'firebase/app';
+// import 'firebase/firestore';
+// import 'firebase/auth'
+import * as firebase from 'firebase';
+
+var config = {
+  apiKey: "AIzaSyAbrPremBQusjYoKqKfTcVHwxuw-Q8ikHw",
+  authDomain: "booked-8586e.firebaseapp.com",
+  databaseURL: "https://booked-8586e.firebaseio.com",
+  projectId: "booked-8586e",
+  storageBucket: "booked-8586e.appspot.com",
+  messagingSenderId: "24720035765",
+  appId: "1:24720035765:web:3d57fd5c840b8100c353fc",
+  measurementId: "G-20QKEY7V1D"
+};
+firebase.initializeApp(config);
+
 class TodoApp extends React.Component {
   constructor(props) {
     super(props);
     this.state = { items: [], text: '' };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount(){
+    const rootRef = firebase.database().ref(); //.child('react')
+    const itemsRef = rootRef.child('text');
+    itemsRef.on('value', snap=> {
+      const newItem = {
+        text: snap.val(),
+        id: Date.now()
+      };
+      //TodoList items=this.state.items;
+      // this.setState({
+      //   //text: snap.val
+      //   items: self.concat(newItem),
+      //   text: ''
+      // });
+      this.setState(state => ({
+        items: state.items.concat(newItem),
+        text: ''
+      }));
+    });
   }
 
   render() {
