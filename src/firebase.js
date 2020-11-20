6b2114069c920c42cf918d03cce8f92e2a97e98c
daniel <danielv1777@yahoo.com>
2020-11-20 00:38:49 -0800

    privacy enhanced and organized code base

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@ import * as firebase from "firebase/app";
 import 'firebase/auth';
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyAbrPremBQusjYoKqKfTcVHwxuw-Q8ikHw",
-  authDomain: "booked-8586e.firebaseapp.com",
-  databaseURL: "https://booked-8586e.firebaseio.com",
-  projectId: "booked-8586e",
-  storageBucket: "booked-8586e.appspot.com",
-  messagingSenderId: "24720035765",
-  appId: "1:24720035765:web:3d57fd5c840b8100c353fc",
-  measurementId: "G-20QKEY7V1D"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 });
 
 export default app;
